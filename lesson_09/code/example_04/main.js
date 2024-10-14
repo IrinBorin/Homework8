ac3bf3e0769ab6e96749ae1e099701ea5f347ad3
Irina Holt <holt.irina@gmail.com>
2024-10-14 12:36:39 +0200

    Delete lesson_09/code directory

--- lesson_09/code/example_04/main.js
+++
@@ -1,7 +0,0 @@
-//String
-const text = "Hello, world! I'm Johny.";
-console.log(text.length);
-const toLower = text.toLocaleLowerCase();
-console.log (toLower);
-const partOfText = text.substring(6, 12);
-console.log (partOfText);
--- lesson_09/code/example_06/main.js
+++
@@ -1,7 +0,0 @@
-const arrOfNumbers = [2, 4, 6, 10, 15];
-const arrOfNumbersMod = [];
-for(let i = 0; i < arrOfNumbers.length; i++){
-  const result = Math.pow(arrOfNumbers[i], 2);
-  console.log(result);
-  arrOfNumbersMod.push(result);
-}
